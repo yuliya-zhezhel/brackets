150ada7f90117cfe0778307daa8b709b602026c1
yuliya-zhezhel <yuliya.zhezhel@gmail.com>
2019-03-13 15:58:41 +0300

    test result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  var stack = [];
+
+  for(let i = 0;i < str.length;i++)
+  {
+    let headStackValue = stack[stack.length-1];
+    
+    if(twoBrackets(headStackValue,str[i],bracketsConfig))
+      stack.pop();
+    else
+    {
+      if(isOpenBreckets(str[i],bracketsConfig) )
+        stack.push(str[i]);
+      else
+        return false;       
+    }
+    
+  }
+
+
+  if(stack.length == 0)
+    return true;
+  else
+    return false
+}
+
+const openBracketI = 0;
+const closeBracketI = 1;
+
+function isOpenBreckets( bracket , bracketsConfig){
+  for(let i=0;i< bracketsConfig.length;i++)
+  {
+    if(bracket == bracketsConfig[i][openBracketI])
+      return true;
+  }
+  return false;
+}
+
+
+function twoBrackets( openBracket , closeBracket, bracketsConfig){
+  for(let i=0;i< bracketsConfig.length;i++)
+  {
+    if(openBracket == bracketsConfig[i][openBracketI])
+      if(closeBracket == bracketsConfig[i][closeBracketI])
+        return true;
+      else
+        return false;
+  }
+  return false;
 }
